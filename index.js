33b858ea48f42766c747655b10d44631d35974e5
Jordan-182 <Jordan.P62@outlook.fr>
2025-06-02 12:10:08 +0200

    workshop done

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+import Fighter from "./src/Fighter.js";
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const fighters = [
+  new Fighter("🧔 Heracles", 20, 6),
+  new Fighter("🦁 Nemean Lion", 11, 13),
+];
+
+const Heracles = fighters[0];
+const Nemean = fighters[1];
+
+function battle(fighter1, fighter2) {
+  let round = 0;
+  while (fighter1.life > 0 && fighter2.life > 0) {
+    console.log(`Round n°${round}`);
+    fighter1.fight(fighter2);
+    round++;
+    if (fighter2.life > 0) {
+      console.log(`Round n°${round}`);
+      fighter2.fight(fighter1);
+    }
+    round++;
+  }
+  return;
+}
+
+battle(Heracles, Nemean);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,32 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+export default class Fighter {
+  constructor(name, strength, dexterity, life = MAX_LIFE) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = life;
+  }
+  fight(Fighter) {
+    const attackPower = Math.floor(Math.random() * (this.strength - 1)) + 1;
+    const damages = Math.max(0, attackPower - Fighter.dexterity);
+    const newLife = Fighter.life - damages;
+    Fighter.life = Math.max(0, newLife);
+    if (Fighter.life > 0) {
+      console.log(
+        `${this.name} attacked ${Fighter.name} and inflicted him ${damages} damages! ${Fighter.name} now has ${Fighter.life}HP`
+      );
+    }
+    if (Fighter.life === 0) {
+      console.log(
+        `${Fighter.name} has been defeated! ${this.name} still has ${this.life}HP`
+      );
+    }
+    if (this.life === 0) {
+      console.log(
+        `${this.name} has been defeated! ${Fighter.name} still has ${Fighter.life}HP`
+      );
+    }
+  }
+}
